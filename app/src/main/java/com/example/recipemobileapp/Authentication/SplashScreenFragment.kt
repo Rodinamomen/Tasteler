1ace6b30bd1ea55d047e14633ca51b7a79b34af4
b2b <Rodinamobark3@gmail.com>
2023-08-01 17:12:56 +0300

    Splash screen

--- app/src/main/java/com/example/recipemobileapp/Authentication/SplashScreenFragment.kt
+++ app/src/main/java/com/example/recipemobileapp/Authentication/SplashScreenFragment.kt
@@ -11,7 +11,6 @@ import com.example.recipemobileapp.R
 
 class SplashScreenFragment : Fragment() {
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
