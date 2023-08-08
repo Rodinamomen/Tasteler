7464959986fa86a1529b618b9b67a61b9dea73a5
b2b <Rodinamobark3@gmail.com>
2023-08-08 18:46:04 +0300

    Sign out

--- app/src/main/java/com/example/recipemobileapp/HomeActivity/HomeActivity.kt
+++ app/src/main/java/com/example/recipemobileapp/HomeActivity/HomeActivity.kt
@@ -1,31 +1,41 @@
 package com.example.recipemobileapp.HomeActivity
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
-import android.widget.Toast
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
-import androidx.navigation.Navigation.findNavController
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import com.example.recipemobileapp.Authentication.Login.view.LoginFragment
 import com.example.recipemobileapp.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import java.security.AccessController.getContext
 
 class HomeActivity : AppCompatActivity() {
+    companion object {
+        const val SHARED_PREFS = "shared_prefs"
+        const val EMAIL_KEY = "email_key"
+        const val PASSWORD_KEY = "password_key"
+    }
+    lateinit var editor: SharedPreferences.Editor
+    lateinit var sharedPreferences: SharedPreferences
+    lateinit var navHostFragment : NavHostFragment
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navHostFragment =
+         navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
-        val navController = navHostFragment.navController
+         navController = navHostFragment.navController
         val navView: BottomNavigationView = findViewById(R.id.bottomnavigationbar)
-
+        val bottomAppbar:CoordinatorLayout = findViewById(R.id.coordinatorLayout)
 
         navView.background = null
         navView.selectedItemId = R.id.placeholder
@@ -36,15 +46,16 @@ class HomeActivity : AppCompatActivity() {
         fab.setOnClickListener {
             navController.navigate(R.id.homeFragment)
             fab.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_home_filled))
-
-
-
-
         }
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.detailsFragment) {
+                bottomAppbar.visibility = View.GONE
 
+            } else {
 
-
-
+                bottomAppbar.visibility = View.VISIBLE
+            }
+        }
 
     }
 
@@ -58,13 +69,29 @@ class HomeActivity : AppCompatActivity() {
 
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navController = navHostFragment.navController
+        sharedPreferences= getSharedPreferences(
+            LoginFragment.SHARED_PREFS,
+            Context.MODE_PRIVATE)
+        editor=sharedPreferences.edit()
         when(item.itemId){
             R.id.aboutFragment ->{
                 val navController = findNavController(R.id.nav_host)
                 navController.navigate(R.id.aboutFragment) }
 
 
-            else -> { Toast.makeText(this,"SignOut was selected",Toast.LENGTH_SHORT).show() } }
+            else -> { sharedPreferences= getSharedPreferences(
+                LoginFragment.SHARED_PREFS,
+                Context.MODE_PRIVATE)
+                editor=sharedPreferences.edit()
+                editor.remove(EMAIL_KEY)
+                editor.remove(PASSWORD_KEY)
+                editor.commit()
+             //   navController.navigate(R.id.aucthenticationActivity)
+                finish()
+            } }
 
         return super.onOptionsItemSelected(item)}
 
